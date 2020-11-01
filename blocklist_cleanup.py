6bf408df1d39c100c6c48e335e55ce2cf144a13c
Jaakko Pallari <jkpl@lepovirta.org>
2020-11-01 14:47:22 +0200

    Whitelist to unblock certain sites

--- blocklist_cleanup.py
+++ blocklist_cleanup.py
@@ -3,6 +3,7 @@
 import sys
 import os
 
+WHITELIST_PATH=os.getenv("WHITELIST_PATH", "whitelist.txt")
 TARGET_IP=os.getenv("TARGET_IP", "0.0.0.0")
 NOT_EXTERNAL_HOSTNAME=set((
     "127.0.0.1",
@@ -11,10 +12,15 @@ NOT_EXTERNAL_HOSTNAME=set((
     "localhost",
 ))
 
+def load_whitelist():
+    with open(WHITELIST_PATH) as fp:
+        lines = (line.strip() for line in fp.readlines())
+        return set(lines)
+
 def is_external_hostname(s):
     return not s in NOT_EXTERNAL_HOSTNAME
 
-def clean_line(line):
+def clean_line(line, whitelist):
     # Filter out commented lines
     if line.startswith("#"):
         return None
@@ -23,16 +29,24 @@ def clean_line(line):
     if line.isspace() or not line:
         return None
 
+    # Extract hostname from the line
     host = next((
         part for part in line.split()
         if is_external_hostname(part)
     ), "")
 
+    # Filter out whitelisted hosts
+    if host in whitelist:
+        return None
+
+    # Print hosts in dnsmasq format
     if host:
         return f"address=/{host}/{TARGET_IP}"
+
     return None
 
 def main(sourcefile, outputfile):
+    whitelist = load_whitelist()
     try:
         if outputfile:
             output = open(outputfile, "a")
@@ -40,7 +54,7 @@ def main(sourcefile, outputfile):
             output = sys.stderr
         with open(sourcefile) as fp:
             for line in fp:
-                cleaned_line = clean_line(line)
+                cleaned_line = clean_line(line, whitelist)
                 if cleaned_line:
                     print(cleaned_line, file=output)
     finally:
