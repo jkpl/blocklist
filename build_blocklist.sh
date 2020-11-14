089080a64a270baa442d5241d05a278d6c0649c5
Jaakko Pallari <jkpl@lepovirta.org>
2020-11-14 16:06:03 +0200

    Custom blocklist sources

--- build_blocklist.sh
+++ build_blocklist.sh
@@ -1,9 +1,44 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SOURCES_URL="https://v.firebog.net/hosts/lists.php?type=tick"
+# Based on https://v.firebog.net/hosts/lists.php?type=tick
+SOURCES='
+https://adaway.org/hosts.txt
+https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt
+https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt
+https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt
+https://mirror.cedia.org.ec/malwaredomains/immortal_domains.txt
+https://mirror1.malwaredomains.com/files/justdomains
+https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt
+https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext
+https://phishing.army/download/phishing_army_blocklist_extended.txt
+https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
+https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
+https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt
+https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts
+https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts_without_controversies.txt
+https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt
+https://urlhaus.abuse.ch/downloads/hostfile/
+https://v.firebog.net/hosts/AdguardDNS.txt
+https://v.firebog.net/hosts/Admiral.txt
+https://v.firebog.net/hosts/Easylist.txt
+https://v.firebog.net/hosts/Easyprivacy.txt
+https://v.firebog.net/hosts/Prigent-Ads.txt
+https://v.firebog.net/hosts/Prigent-Crypto.txt
+https://v.firebog.net/hosts/Shalla-mal.txt
+https://v.firebog.net/hosts/static/w3kbl.txt
+https://www.malwaredomainlist.com/hostslist/hosts.txt
+https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser
+'
+SOURCES=$(echo "${SOURCES}" | sed '/^$/d') # trim empty lines
+
 TARGET_DIR="$(mktemp -d tmp.XXXXX)"
-SOURCES_FILE="${TARGET_DIR}/sources.txt"
 FINAL_BLOCKLIST="blocklist.txt"
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -18,13 +53,8 @@ mkdir -p \
     "${TARGET_DIR}/raw" \
     "${TARGET_DIR}/cleaned"
 
-if [ ! -f "${SOURCES_FILE}" ]; then
-    echo "Fetching source list" >&2
-    "${CURDIR}/download_file.sh" "${SOURCES_URL}" "${SOURCES_FILE}"
-fi
-
 echo "Fetching blocklists" >&2
-parallel "${CURDIR}/download_file.sh" {} "${TARGET_DIR}/raw/{#}.txt" < "${SOURCES_FILE}"
+parallel "${CURDIR}/download_file.sh" {} "${TARGET_DIR}/raw/{#}.txt" ::: "${SOURCES}"
 
 echo "Cleaning up blocklists" >&2
 parallel "${CURDIR}/blocklist_cleanup.py" {} "${TARGET_DIR}/cleaned/{/}.txt" ::: "${TARGET_DIR}/raw"/*.txt
@@ -34,8 +64,7 @@ sort --unique "${TARGET_DIR}/cleaned"/*.txt > "${FINAL_BLOCKLIST}"
 
 cat <<EOF >&2
 Stats:
-  Sources crawled : $(wc -l < "${SOURCES_FILE}")
+  Sources crawled : $(echo "${SOURCES}" | wc -l)
   Hosts blocked   : $(wc -l < "${FINAL_BLOCKLIST}")
   File size       : $(du -h "${FINAL_BLOCKLIST}" | awk '{print $1}')
 EOF
-
